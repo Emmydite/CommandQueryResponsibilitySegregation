2ff6aefdbf0059b760e11db54c917d8f0922cebd
Emmanuel <emmacomessi@gmail.com>
2023-02-05 10:04:00 +0000

    Added application dbContext and product model class

---
+++ CommandQueryResponsibilitySegregation/Models/Product.cs
@@ -0,0 +1,10 @@
+﻿namespace CommandQueryResponsibilitySegregation.Models
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        public string ProductName { get; set; }
+        public string ProductSKU { get; set; }
+        public decimal Price { get; set; }
+    }
+}
