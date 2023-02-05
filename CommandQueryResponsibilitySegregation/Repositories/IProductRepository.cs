b9eef8d75103887232bc113d951553aa38f9169b
Emmanuel <emmacomessi@gmail.com>
2023-02-05 14:43:22 +0000

    added product interface endpoints

--- CommandQueryResponsibilitySegregation/Repositories/IProductRepository.cs
+++ CommandQueryResponsibilitySegregation/Repositories/IProductRepository.cs
@@ -1,7 +1,13 @@
-﻿namespace CommandQueryResponsibilitySegregation.Repositories
+﻿using CommandQueryResponsibilitySegregation.Models;
+
+namespace CommandQueryResponsibilitySegregation.Repositories
 {
     public interface IProductRepository
     {
-
+        public Task<List<Product>> ProductList();
+        public Task<Product> GetProductById(int Id);
+        public Task<Product> AddProduct(Product studentDetails);
+        public Task<int> UpdateProduct(Product studentDetails);
+        public Task<int> DeleteProduct(int Id);
     }
 }
