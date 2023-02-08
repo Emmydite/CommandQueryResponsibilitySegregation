8cc58650ee0048ac5b8a1fa251defeebb26ff2ad
Emmanuel <emmacomessi@gmail.com>
2023-02-08 09:25:45 +0000

    added create product command properties

--- CommandQueryResponsibilitySegregation/Commands/CreateProductCommand.cs
+++ CommandQueryResponsibilitySegregation/Commands/CreateProductCommand.cs
@@ -2,6 +2,10 @@
 {
     public class CreateProductCommand
     {
+        public string ProductName { get; set; }
+        public string ProductSKU { get; set; }
+        public decimal Price { get; set; }
+
 
     }
 }
