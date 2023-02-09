6dc839c1220eb29aae2f8b3ed782ddcfef5a76d1
Emmanuel <emmacomessi@gmail.com>
2023-02-09 12:41:30 +0000

    refactored

--- CommandQueryResponsibilitySegregation/Commands/CreateProductCommand.cs
+++ CommandQueryResponsibilitySegregation/Commands/CreateProductCommand.cs
@@ -5,6 +5,7 @@ namespace CommandQueryResponsibilitySegregation.Commands
 {
     public class CreateProductCommand : IRequest<Product>
     {
+        public int Id { get; set; }
         public string ProductName { get; set; }
         public string ProductSKU { get; set; }
         public decimal Price { get; set; }
