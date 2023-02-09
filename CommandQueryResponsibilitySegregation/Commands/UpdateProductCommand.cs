a9d111ecbe268d97c4e5350dde32deec48013f47
Emmanuel <emmacomessi@gmail.com>
2023-02-09 12:40:11 +0000

    Added update product command fields

--- CommandQueryResponsibilitySegregation/Commands/UpdateProductCommand.cs
+++ CommandQueryResponsibilitySegregation/Commands/UpdateProductCommand.cs
@@ -2,5 +2,9 @@
 {
     public class UpdateProductCommand
     {
+        public int Id { get; set; }
+        public string ProductName { get; set; }
+        public string ProductSKU { get; set; }
+        public decimal Price { get; set; }
     }
 }
