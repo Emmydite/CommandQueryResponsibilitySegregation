f1aa0fc698ff8f88a7636f99db3473c04a723504
Emmanuel <emmacomessi@gmail.com>
2023-02-09 12:43:05 +0000

    removed the Id product field as it's the primary key and will be auto increamented by the db

--- CommandQueryResponsibilitySegregation/Commands/CreateProductCommand.cs
+++ CommandQueryResponsibilitySegregation/Commands/CreateProductCommand.cs
@@ -5,7 +5,6 @@ namespace CommandQueryResponsibilitySegregation.Commands
 {
     public class CreateProductCommand : IRequest<Product>
     {
-        public int Id { get; set; }
         public string ProductName { get; set; }
         public string ProductSKU { get; set; }
         public decimal Price { get; set; }
