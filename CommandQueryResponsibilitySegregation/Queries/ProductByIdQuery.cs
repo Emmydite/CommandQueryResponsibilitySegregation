87a2e17c8c3b5b1e1fde705ad08dd472736a2694
Emmanuel <emmacomessi@gmail.com>
2023-02-13 13:05:03 +0000

    add Id field for getting product by Id

--- CommandQueryResponsibilitySegregation/Queries/ProductByIdQuery.cs
+++ CommandQueryResponsibilitySegregation/Queries/ProductByIdQuery.cs
@@ -1,6 +1,8 @@
-﻿namespace CommandQueryResponsibilitySegregation.Queries
+﻿
+namespace CommandQueryResponsibilitySegregation.Queries
 {
     public class ProductByIdQuery
     {
+        public int Id { get; set; }
     }
 }
