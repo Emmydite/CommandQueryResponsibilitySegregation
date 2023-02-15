fed68a360d4cefa6d6aa7224cc6bed81c870e5c2
Emmanuel <emmacomessi@gmail.com>
2023-02-15 17:08:32 +0000

    Implemented the MediatR IRequestHandler

--- CommandQueryResponsibilitySegregation/Handlers/CreateProductHandler.cs
+++ CommandQueryResponsibilitySegregation/Handlers/CreateProductHandler.cs
@@ -1,13 +1,21 @@
-﻿using CommandQueryResponsibilitySegregation.Repositories;
+﻿using CommandQueryResponsibilitySegregation.Commands;
+using CommandQueryResponsibilitySegregation.Models;
+using CommandQueryResponsibilitySegregation.Repositories;
+using MediatR;
 
 namespace CommandQueryResponsibilitySegregation.Handlers
 {
-    public class CreateProductHandler
+    public class CreateProductHandler : IRequestHandler<CreateProductCommand, Product>
     {
         private readonly IProductRepository _productRepository;
         public CreateProductHandler(IProductRepository productRepository)
         {
             _productRepository = productRepository;
+        }
+
+        public Task<Product> Handle(CreateProductCommand request, CancellationToken cancellationToken)
+        {
+            throw new NotImplementedException();
         }
     }
 }
