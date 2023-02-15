4bd4c73c71441f40f279bb7fd1afdc67006f6036
Emmanuel <emmacomessi@gmail.com>
2023-02-15 17:06:03 +0000

    decleared a private product repository field and initialized at the constructor level

--- CommandQueryResponsibilitySegregation/Handlers/CreateProductHandler.cs
+++ CommandQueryResponsibilitySegregation/Handlers/CreateProductHandler.cs
@@ -1,6 +1,13 @@
-﻿namespace CommandQueryResponsibilitySegregation.Handlers
+﻿using CommandQueryResponsibilitySegregation.Repositories;
+
+namespace CommandQueryResponsibilitySegregation.Handlers
 {
     public class CreateProductHandler
     {
+        private readonly IProductRepository _productRepository;
+        public CreateProductHandler(IProductRepository productRepository)
+        {
+            _productRepository = productRepository;
+        }
     }
 }
