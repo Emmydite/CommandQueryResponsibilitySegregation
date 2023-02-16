e08b23de70a2eb0443bddfaa74745df9a51c60a4
Emmanuel <emmacomessi@gmail.com>
2023-02-16 13:04:19 +0000

    added product repository interface and initialized at constructor

--- CommandQueryResponsibilitySegregation/Handlers/DeleteProductHandler.cs
+++ CommandQueryResponsibilitySegregation/Handlers/DeleteProductHandler.cs
@@ -1,6 +1,13 @@
-﻿namespace CommandQueryResponsibilitySegregation.Handlers
+﻿using CommandQueryResponsibilitySegregation.Repositories;
+
+namespace CommandQueryResponsibilitySegregation.Handlers
 {
     public class DeleteProductHandler
     {
+        private readonly IProductRepository _productRepository;
+        public DeleteProductHandler(IProductRepository productRepository)
+        {
+            _productRepository = productRepository
+        }
     }
 }
