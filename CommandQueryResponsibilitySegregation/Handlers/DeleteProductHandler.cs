1a8cf44d70b60a0814c26797b84da82729490b17
Emmanuel <emmacomessi@gmail.com>
2023-02-17 18:02:17 +0000

    Implemented the Request handler interface

--- CommandQueryResponsibilitySegregation/Handlers/DeleteProductHandler.cs
+++ CommandQueryResponsibilitySegregation/Handlers/DeleteProductHandler.cs
@@ -1,13 +1,20 @@
-﻿using CommandQueryResponsibilitySegregation.Repositories;
+﻿using CommandQueryResponsibilitySegregation.Commands;
+using CommandQueryResponsibilitySegregation.Repositories;
+using MediatR;
 
 namespace CommandQueryResponsibilitySegregation.Handlers
 {
-    public class DeleteProductHandler
+    public class DeleteProductHandler : IRequestHandler<DeleteProductCommand, int>
     {
         private readonly IProductRepository _productRepository;
         public DeleteProductHandler(IProductRepository productRepository)
         {
             _productRepository = productRepository;
+        }
+
+        public Task<int> Handle(DeleteProductCommand request, CancellationToken cancellationToken)
+        {
+            throw new NotImplementedException();
         }
     }
 }
