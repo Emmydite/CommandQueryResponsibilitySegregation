02f2b9e462eed3df6437ca290c099b2dee0434fb
Emmanuel <emmacomessi@gmail.com>
2023-02-19 13:09:13 +0000

    Inherited the IRequest interface

--- CommandQueryResponsibilitySegregation/Handlers/UpdateProductHandler.cs
+++ CommandQueryResponsibilitySegregation/Handlers/UpdateProductHandler.cs
@@ -1,13 +1,17 @@
-﻿using CommandQueryResponsibilitySegregation.Repositories;
+﻿using CommandQueryResponsibilitySegregation.Commands;
+using CommandQueryResponsibilitySegregation.Repositories;
+using MediatR;
 
 namespace CommandQueryResponsibilitySegregation.Handlers
 {
-    public class UpdateProductHandler
+    public class UpdateProductHandler : IRequestHandler<UpdateProductCommand, int>
     {
         private readonly IProductRepository _productRepository;
         public UpdateProductHandler(IProductRepository productRepository)
         {
             _productRepository = productRepository;
         }
+
+
     }
 }
