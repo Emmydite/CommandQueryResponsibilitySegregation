af20f950be48ea2926abd06bb2a279f043cf991d
Emmanuel <emmacomessi@gmail.com>
2023-02-22 18:02:51 +0000

    Inherited the IRequestHandler

--- CommandQueryResponsibilitySegregation/Handlers/GetProductByIdHandler.cs
+++ CommandQueryResponsibilitySegregation/Handlers/GetProductByIdHandler.cs
@@ -1,13 +1,18 @@
-﻿using CommandQueryResponsibilitySegregation.Repositories;
+﻿using CommandQueryResponsibilitySegregation.Models;
+using CommandQueryResponsibilitySegregation.Queries;
+using CommandQueryResponsibilitySegregation.Repositories;
+using MediatR;
 
 namespace CommandQueryResponsibilitySegregation.Handlers
 {
-    public class GetProductByIdHandler
+    public class GetProductByIdHandler : IRequestHandler<ProductByIdQuery, Product>
     {
         private readonly IProductRepository _productRepository;
         public GetProductByIdHandler(IProductRepository productRepository)
         {
             _productRepository = productRepository;
         }
+
+
     }
 }
