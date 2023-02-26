9bc1ae29667890474dcd8b9786871b028671b018
Emmanuel <emmacomessi@gmail.com>
2023-02-26 13:15:00 +0000

    Added MediatR service to the container

--- CommandQueryResponsibilitySegregation/Program.cs
+++ CommandQueryResponsibilitySegregation/Program.cs
@@ -1,6 +1,10 @@
+
+using System.Reflection;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+builder.Services.AddMediatR(Assembly.GetExecutingAssembly());
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
