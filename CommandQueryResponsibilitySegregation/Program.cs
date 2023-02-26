70dff51737a3743edd57d5923b925fa444e0df9c
Emmanuel <emmacomessi@gmail.com>
2023-02-26 13:19:04 +0000

    added the repository service class container scope service lifetime

--- CommandQueryResponsibilitySegregation/Program.cs
+++ CommandQueryResponsibilitySegregation/Program.cs
@@ -1,4 +1,5 @@
 using CommandQueryResponsibilitySegregation.Data;
+using CommandQueryResponsibilitySegregation.Repositories;
 using MediatR;
 using System.Reflection;
 
@@ -7,6 +8,7 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 builder.Services.AddMediatR(Assembly.GetExecutingAssembly());
 builder.Services.AddDbContext<ApplicationDbContext>();
+builder.Services.AddScoped<IProductRepository, ProductRepository>();
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
